7cac3f200acd8b01244a82d4aa05d1c6b764623d
Eimilly__S2 <emillysantosaaraujo@gmail.com>
2026-03-03 18:33:42 -0300

    Add files via upload
    7.                  Receba os valores do comprimento, largura e altura de um paralelepípedo. Calcule e mostre seu volume.

---
+++ Questão7.py
@@ -0,0 +1,16 @@
+#Declarar
+
+Largura : int = 0
+Altura : int = 0
+Comprimento : int = 0
+Volume : int = 0
+
+#Inicio
+
+Largura = int (input("Digite o valor da Largura do Paralelepípedo: "))
+Altura = int (input("Digite o valor da Altura Paralelepípedo: "))
+Comprimento = int (input ("Digite o valor do Comprimento Paralelepípedo: "))
+Volume = (Comprimento * Largura * Altura)
+print ("O Volume do seu Paralelepípedo é: ",Volume)
+
+#Fim
